95705088e65e4d7b3a6d35f13ac18074e3c02de5
WaleedHassan <waleed46shah@gmail.com>
2023-01-21 11:31:06 +0500

    Sanity Server Setup With Schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import testimonials from "./testimonials";
+import about from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+
+export const schemaTypes = [about,brands,contact,experiences,testimonials,skills,workExperience,works]
